1b9162973717698e8426715ecd3a7bbc8d1590e9
Jean-Maxime Couillard <jeanmaximecouillard@gmail.com>
2017-01-02 19:25:27 -0500

    Increased zoom level by one, updated timestamp label (time ago), smaller pin image for better 2x resolution.

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ $lastData = file_get_contents("last.txt");
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
 	<link rel="stylesheet" href="main.css">
 </head>
 <body>
 <div id="map"></div>
-<div id="locating"><span>Relocation en cours...</span></div>
+<div id="locating"><span>Relocalisation en cours...</span></div>
+<time id="timeago"></time>
 <script>
 
 	var map, marker;
@@ -24,11 +25,11 @@ $lastData = file_get_contents("last.txt");
 		map = new google.maps.Map(document.getElementById('map'), {
 			scrollwheel: false,
 			center: myLatlng,
-			zoom: 10
+			zoom: 11
 		});
 
 		var image = {
-			url: 'custompin.png',
+			url: 'custompin_s.png',
 			scaledSize: new google.maps.Size(34, 55)
 		};
 
@@ -38,11 +39,14 @@ $lastData = file_get_contents("last.txt");
 			icon: image
 		});
 
+		updateTimestamp(lastData.timeStamp);
+		jQuery("#timeago").timeago();
+
 		setInterval(updateMap, updateDelay * 1000);
 		updateMap();
 	}
 
-	function updateMap(){
+	function updateMap() {
 		$.ajax({
 			dataType: "json",
 			url: "api.php",
@@ -65,13 +69,22 @@ $lastData = file_get_contents("last.txt");
 		marker.setPosition(myLatlng);
 		map.setCenter(myLatlng);
 
+		updateTimestamp(data.timeStamp);
+
 		jQuery("#locating").fadeOut();
 
+	}
+
+	function updateTimestamp(timeStamp) {
+		var time = new Date(timeStamp).toISOString();
+		jQuery("#timeago").attr("datetime", time).timeago("update", time);
 	}
 
 
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="jquery.timeago.js"></script>
+<script src="jquery.timeago.fr.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=<?= GMAPS_API_KEY ?>&callback=initMap"
 		async defer></script>
 </body>
